8d7c21e4c437f77a77f84f4ee75ac837293b5c52
Mohmaed Zribi <Mohmaed.Zribi.dr@gmail.com>
2022-06-30 13:44:53 +0100

    first commit

--- base/views.py
+++ base/views.py
@@ -46,10 +46,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False)
 
-        search_input = self.request.GET.get('search-are') or ''
+        search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(
-                title_icontains=search_input)
+                title__startswith=search_input)
+        
+        context['search_input']  = search_input
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
